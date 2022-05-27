f01b2740c576d2389e5e1b3fe8bb874c82f2662b
kuzan-ux <oghenetegafrancis@gmail.com>
2022-05-27 22:51:29 +0100

    First Commit

---
+++ Counter_App/script.js
@@ -0,0 +1,24 @@
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+
+let count = 0
+
+function increCount() {
+    count += 1
+    countEl.textContent = count
+    console.log(count)
+}
+
+function decreCount() {
+    count -= 1
+    countEl.textContent = count
+    console.log(count)
+}
+
+function save() {
+    let countStr = count + ", "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
